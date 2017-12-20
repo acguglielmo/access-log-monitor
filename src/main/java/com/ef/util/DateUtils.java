e89b2517e8beaae253929f4aa62260123e6c7fa0
antonion <antonion@ciandt.com>
2017-12-20 14:44:56 -0200

    Improvement required by singleton pattern.

--- src/main/java/com/ef/util/DateUtils.java
+++ src/main/java/com/ef/util/DateUtils.java
@@ -9,7 +9,7 @@ public final class DateUtils {
     public static final DateTimeFormatter DATE_FORMAT_FILE = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
     public static final DateTimeFormatter DATE_FORMAT_ARGS = DateTimeFormatter.ofPattern("yyyy-MM-dd.HH:mm:ss");
 
-    private static DateUtils instance;
+    private volatile static DateUtils instance;
 
     private DateUtils(){}
 
