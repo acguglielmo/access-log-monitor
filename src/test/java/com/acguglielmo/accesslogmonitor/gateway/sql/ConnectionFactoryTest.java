3b6d03c1aa4a80e1244755f42e7d1ef4b7509f29
Antônio Carlos Guglielmo Neto <antonio_guglielmo@hotmail.com>
2020-05-23 20:06:51 -0300

    Fix config file used in tests

--- src/test/java/com/acguglielmo/accesslogmonitor/gateway/sql/ConnectionFactoryTest.java
+++ src/test/java/com/acguglielmo/accesslogmonitor/gateway/sql/ConnectionFactoryTest.java
@@ -1,37 +1,18 @@
 package com.acguglielmo.accesslogmonitor.gateway.sql;
 
-import com.acguglielmo.accesslogmonitor.util.PropertiesHolder;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
-
-import java.io.BufferedWriter;
-import java.nio.file.*;
 import java.sql.Connection;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
+
+import com.acguglielmo.accesslogmonitor.util.PropertiesHolder;
 
 public class ConnectionFactoryTest {
 
-    private static final String CONFIG_FILENAME = "config.properties";
-
-    @BeforeClass
-    public static void setUp() throws Exception {
-        final Path path = Paths.get(CONFIG_FILENAME);
-        if (Files.exists(path, LinkOption.NOFOLLOW_LINKS)) {
-            Files.delete(path);
-        }
-
-        final Path filePath = Files.createFile(path);
-        final BufferedWriter bufferedWriter = Files.newBufferedWriter(filePath, StandardOpenOption.WRITE);
-
-        bufferedWriter.write("\n" + PropertiesHolder.DB_CONNECTION_URL + "=jdbc:hsqldb:mem:test"+
-                "\n" + PropertiesHolder.DB_CONNECTION_USERNAME + "=sa"+
-                "\n" + PropertiesHolder.DB_CONNECTION_PASSWORD + "=");
-
-        bufferedWriter.close();
-    }
+    private static final String CONFIG_FILENAME = "src/test/resources/application.properties";
 
     @Test
     public void getInstanceTest() throws Exception {
@@ -51,14 +32,6 @@ public class ConnectionFactoryTest {
 
         assertNotNull(connection);
         assertNotEquals(connection, ConnectionFactory.getInstance().getConnection());
-    }
-
-    @AfterClass
-    public static void tearDown() throws Exception {
-        final Path path = Paths.get(CONFIG_FILENAME);
-        if (Files.exists(path, LinkOption.NOFOLLOW_LINKS)) {
-            Files.delete(path);
-        }
     }
 
 }
