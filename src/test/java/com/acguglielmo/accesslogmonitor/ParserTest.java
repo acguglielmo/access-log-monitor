c6336823ee38aab9664a87369ab8c838ffb215ec
Antônio Carlos Guglielmo Neto <antonio_guglielmo@hotmail.com>
2020-05-23 17:36:56 -0300

    Improve unitary test

--- src/test/java/com/acguglielmo/accesslogmonitor/ParserTest.java
+++ src/test/java/com/acguglielmo/accesslogmonitor/ParserTest.java
@@ -1,19 +1,59 @@
 package com.acguglielmo.accesslogmonitor;
 
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertEquals;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ParserTest {
 
-	@Test
-	public void shouldDoNothingWhenNoArgsAreProvidedTest() {
+	private final PrintStream originalOut = System.out;
+	
+	private final PrintStream originalErr = System.err;
 
-		final String[] args = null;
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+	
+	private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+	
+	@Before
+	public void before() {
 		
-		Parser.main(args);
+		System.setOut(new PrintStream(outContent));
 		
-		assertNull(args);
+		System.setErr(new PrintStream(errContent));
+		
+	}
+
+	@After
+	public void after() {
+
+		System.setOut(originalOut);
+
+	    System.setErr(originalErr);
+
+	}
+	
+	@Test
+	public void shouldShowHelpMessageWhenNoArgsAreProvidedTest() {
+
+		Parser.main(null);
+		
+		assertEquals("usage: parser\r\n" + 
+			" -a,--accessLog <arg>    Path to log file. Default value is access.log (in\r\n" + 
+			"                         the working directory)\r\n" + 
+			" -c,--configFile <arg>   Path to config file. Default value is\r\n" + 
+			"                         config.properties (in the working directory)\r\n" + 
+			" -d,--duration <arg>     Required. Options: [hourly, daily]\r\n" + 
+			" -s,--startDate <arg>    Required. Start date to analysis in the following\r\n" + 
+			"                         format: yyyy-MM-dd.HH:mm:ss\r\n" + 
+			" -t,--threshold <arg>    Required. Threshold value to block. Only integer\r\n" + 
+			"                         values.\r\n" + 
+			"", 
+			outContent.toString());
 		
 	}
 	
