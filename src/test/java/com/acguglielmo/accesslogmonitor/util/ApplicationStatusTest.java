00058b4497e6db8ee6125a5d91f323ae64fbfff1
Antônio Carlos Guglielmo Neto <antonio_guglielmo@hotmail.com>
2020-05-23 21:54:15 -0300

    [WIP] Add test to be used in file

--- src/test/java/com/acguglielmo/accesslogmonitor/util/ApplicationStatusTest.java
+++ src/test/java/com/acguglielmo/accesslogmonitor/util/ApplicationStatusTest.java
@@ -1,11 +1,8 @@
 package com.acguglielmo.accesslogmonitor.util;
 
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
-import java.io.BufferedWriter;
 import java.io.File;
 import java.nio.file.*;
 import java.util.List;
@@ -15,28 +12,9 @@ import java.util.concurrent.Future;
 
 public class ApplicationStatusTest {
 
-    private static final String TEST_FILENAME = "test.txt";
-
-    private File file;
-    private int linesWrittenToTheFile;
-
-    @Before
-    public void setUp() throws Exception {
-        final Path path = Paths.get(TEST_FILENAME);
-        if (Files.exists(path, LinkOption.NOFOLLOW_LINKS)) {
-            Files.delete(path);
-        }
-
-        final Path filePath = Files.createFile(path);
-        final BufferedWriter bufferedWriter = Files.newBufferedWriter(filePath, StandardOpenOption.WRITE);
-
-        for (int i =0; i < 1000; i++) {
-            bufferedWriter.write("\nA line in the text file.");
-            linesWrittenToTheFile++;
-        }
-        bufferedWriter.close();
-        this.file = filePath.toFile();
-    }
+    private File file = Paths.get("src/test/resources/test.txt").toFile();
+    
+    private int linesWrittenToTheFile = 100;
 
     @Test
     public void getInstanceTest() throws Exception {
@@ -56,17 +34,17 @@ public class ApplicationStatusTest {
 
         instance.updateProgressByChunk();
         Assert.assertEquals(1.0 * ApplicationStatus.ESTIMATED_FILE_LOADING_TO_DATABASE_JOB_PERCENTAGE / 100,
-                instance.getProgress(), 0.0001);
+                instance.getProgress(), 0.001);
 
         for (int i = 1; i < 50; i++) {
             instance.updateProgressByChunk();
         }
         Assert.assertEquals(50.0 * ApplicationStatus.ESTIMATED_FILE_LOADING_TO_DATABASE_JOB_PERCENTAGE / 100,
-                instance.getProgress(), 0.0001);
+                instance.getProgress(), 0.001);
 
         instance.updateProgressByChunk();
         Assert.assertEquals(51.0 * ApplicationStatus.ESTIMATED_FILE_LOADING_TO_DATABASE_JOB_PERCENTAGE / 100,
-                instance.getProgress(), 0.0001);
+                instance.getProgress(), 0.001);
     }
 
     @Test
@@ -76,17 +54,17 @@ public class ApplicationStatusTest {
         int batchChunkSize = 10;
         instance.configureChunkSize(file, batchChunkSize);
         Assert.assertEquals((double) batchChunkSize / linesWrittenToTheFile * ApplicationStatus.ESTIMATED_FILE_LOADING_TO_DATABASE_JOB_PERCENTAGE,
-                instance.getChunkSize(), 0.0001);
+                instance.getChunkSize(), 0.001);
 
         batchChunkSize = 20;
         instance.configureChunkSize(file, batchChunkSize);
         Assert.assertEquals((double) batchChunkSize / linesWrittenToTheFile * ApplicationStatus.ESTIMATED_FILE_LOADING_TO_DATABASE_JOB_PERCENTAGE,
-                instance.getChunkSize(), 0.0001);
+                instance.getChunkSize(), 0.001);
 
         batchChunkSize = 30;
         instance.configureChunkSize(file, batchChunkSize);
         Assert.assertEquals((double) batchChunkSize / linesWrittenToTheFile * ApplicationStatus.ESTIMATED_FILE_LOADING_TO_DATABASE_JOB_PERCENTAGE,
-                instance.getChunkSize(), 0.0001);
+                instance.getChunkSize(), 0.001);
 
         batchChunkSize = 400;
         instance.configureChunkSize(file, batchChunkSize);
@@ -144,11 +122,4 @@ public class ApplicationStatusTest {
         executorService.shutdownNow();
     }
 
-    @After
-    public void tearDown() throws Exception {
-        final Path path = Paths.get(TEST_FILENAME);
-        if (Files.exists(path, LinkOption.NOFOLLOW_LINKS)) {
-            Files.delete(path);
-        }
-    }
 }
