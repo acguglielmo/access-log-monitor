f5cd92a4c74257ec1634a8a6106cf4a09fd14281
Antônio Carlos Guglielmo Neto <antonio_guglielmo@hotmail.com>
2020-05-23 21:28:50 -0300

    Refactor FileParserTest.java

--- src/test/java/com/acguglielmo/accesslogmonitor/parser/FileParserTest.java
+++ src/test/java/com/acguglielmo/accesslogmonitor/parser/FileParserTest.java
@@ -11,27 +11,14 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.concurrent.Future;
 
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 
 import com.acguglielmo.accesslogmonitor.gateway.sql.impl.AccessLogGatewaySqlImpl;
 import com.acguglielmo.accesslogmonitor.util.ApplicationStatus;
 
-@RunWith(PowerMockRunner.class)
-@PrepareForTest( {FileParser.class, FileParser.GatewayClient.class})
 public class FileParserTest {
 
-    private static FileParser instance;
-
     private static final String ACCESS_LOG_FILENAME = "src/test/resources/access.log";
-
-    @BeforeClass
-    public static void setUp() throws Exception {
-        instance = new FileParser();
-    }
 
     @Test
     public void loadFileToDatabaseTest() throws Exception {
@@ -42,7 +29,7 @@ public class FileParserTest {
 
         replay(mockedGateway, AccessLogGatewaySqlImpl.class);
 
-        instance.loadFileToDatabase(Paths.get(ACCESS_LOG_FILENAME).toFile());
+        new FileParser().loadFileToDatabase(Paths.get(ACCESS_LOG_FILENAME).toFile());
 
         final List<Future<?>> futureList = ApplicationStatus.getInstance().getFutureList();
         assertFalse(futureList.isEmpty());
