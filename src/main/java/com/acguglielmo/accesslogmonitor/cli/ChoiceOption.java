03c53f97302feccc064b002547b7b46302bb92a4
Antônio Carlos Guglielmo Neto <antonio_guglielmo@hotmail.com>
2020-05-26 17:12:52 -0300

    Fix identation on ChoiceOption.java

--- src/main/java/com/acguglielmo/accesslogmonitor/cli/ChoiceOption.java
+++ src/main/java/com/acguglielmo/accesslogmonitor/cli/ChoiceOption.java
@@ -5,17 +5,18 @@ import org.apache.commons.cli.Option;
 import java.util.Arrays;
 
 public class ChoiceOption extends Option {
-	private static final long serialVersionUID = 723485642511703539L;
-	
-	private final String[] choices;
+    private static final long serialVersionUID = 723485642511703539L;
+
+    private final String[] choices;
 
     ChoiceOption(
-            final String opt,
-            final String longOpt,
-            final boolean hasArg,
-            final String description,
-            boolean isRequired,
-            final String... choices) throws IllegalArgumentException {
+        final String opt,
+        final String longOpt,
+        final boolean hasArg,
+        final String description,
+        boolean isRequired,
+        final String... choices) throws IllegalArgumentException {
+
         super(opt, longOpt, hasArg, description + ' ' + Arrays.toString(choices));
         super.setRequired(isRequired);
         this.choices = choices;
