f13db1b1d92d4a0c54f7ee9b65f143660d32a231
Antônio Carlos Guglielmo Neto <antonio_guglielmo@hotmail.com>
2020-05-31 20:24:15 -0300

    Cleanup ParserTest

--- src/test/java/com/acguglielmo/accesslogmonitor/ParserTest.java
+++ src/test/java/com/acguglielmo/accesslogmonitor/ParserTest.java
@@ -2,6 +2,7 @@ package com.acguglielmo.accesslogmonitor;
 
 import static java.lang.String.format;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import java.util.Optional;
@@ -14,22 +15,19 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import com.acguglielmo.accesslogmonitor.cli.ApplicationCommandLine;
 import com.acguglielmo.accesslogmonitor.cli.CommandLineHelper;
-import com.acguglielmo.accesslogmonitor.gateway.sql.impl.AccessLogGatewaySqlImpl;
-import com.acguglielmo.accesslogmonitor.gateway.sql.impl.BlockOccurrencesGatewaySqlImpl;
+import com.acguglielmo.accesslogmonitor.util.ApplicationStatus;
 import com.acguglielmo.accesslogmonitor.util.PropertiesHolder;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ParserTest {
 
-	@Mock
-	private AccessLogGatewaySqlImpl accessLogGatewaySqlImpl;
-
-	@Mock
-	private BlockOccurrencesGatewaySqlImpl blockOccurrencesGatewaySqlImpl;
+	@Spy
+	private ApplicationStatus applicationStatus;
 	
 	@Mock
 	private CommandLineHelper commandLineHelper;
@@ -58,18 +56,15 @@ public class ParserTest {
 	@Test
 	public void shouldShowConfigFileNotFoundWhenNoConfigFileIsProvidedTest() {
 		
-		final String[] args = 
-			new String[] {"--accessLog=access.log", "--configFile=config.properties", "--startDate=2017-01-01.00:00:00", "--duration=daily", "--threshold=500"};
-
 		final ApplicationCommandLine commandLine = Mockito.mock(ApplicationCommandLine.class);
 		
-		when(commandLineHelper.configureCliOptions(args))
+		when(commandLineHelper.configureCliOptions(any()))
 			.thenReturn(Optional.of(commandLine));
 		
 		when(commandLine.getOptionValue(CommandLineHelper.CONFIG_FILE_PATH, CommandLineHelper.CONFIG_FILE_DEFAULT_VALUE))
 			.thenReturn("path");
 		
-		instance.process(args);
+		instance.process(new String[] {});
 		
 		assertEquals(
 		  format("%s%n", Parser.CONFIG_FILE_NOT_FOUND_MESSAGE), 
