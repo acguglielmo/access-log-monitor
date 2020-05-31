1847122496a7a8835fe4650d554d77b5d3185922
Antônio Carlos Guglielmo Neto <antonio_guglielmo@hotmail.com>
2020-05-31 20:46:11 -0300

    Add more tests to ParserTest

--- src/test/java/com/acguglielmo/accesslogmonitor/ParserTest.java
+++ src/test/java/com/acguglielmo/accesslogmonitor/ParserTest.java
@@ -3,18 +3,23 @@ package com.acguglielmo.accesslogmonitor;
 import static java.lang.String.format;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 import org.apache.logging.log4j.LogManager;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 
@@ -54,15 +59,9 @@ public class ParserTest {
 	}
 	
 	@Test
-	public void shouldShowConfigFileNotFoundWhenNoConfigFileIsProvidedTest() {
+	public void shouldShowConfigFileNotFoundWhenNoConfigFileIsNotFoundTest() {
 		
-		final ApplicationCommandLine commandLine = Mockito.mock(ApplicationCommandLine.class);
-		
-		when(commandLineHelper.configureCliOptions(any()))
-			.thenReturn(Optional.of(commandLine));
-		
-		when(commandLine.getOptionValue(CommandLineHelper.CONFIG_FILE_PATH, CommandLineHelper.CONFIG_FILE_DEFAULT_VALUE))
-			.thenReturn("path");
+		prepareCommandLineMockBehavior("a path");
 		
 		instance.process(new String[] {});
 		
@@ -72,5 +71,37 @@ public class ParserTest {
 		);
 		
 	}
-	
+
+	@Test
+	@Ignore("This test forces a call to System.exit, breaking the build")
+	public void shouldHaltApplicationIfAnExceptionIsThrownByFileParsingTask() throws Exception {
+
+		prepareCommandLineMockBehavior("src/test/resources/application.properties");
+
+		final Future<?> aFuture = mock(Future.class);
+
+		when(aFuture.isDone()).thenReturn(true);
+
+		when(aFuture.get()).thenThrow(new ExecutionException(new RuntimeException("error")));
+
+		final List<Future<?>> futureList = Collections.singletonList(aFuture);
+
+		when(applicationStatus.getFutureList())
+			.thenReturn(futureList);
+
+		instance.process(new String[] {});
+
+	}
+
+	private void prepareCommandLineMockBehavior(final String configFilePath) {
+
+		final ApplicationCommandLine commandLine = mock(ApplicationCommandLine.class);
+		
+		when(commandLineHelper.configureCliOptions(any()))
+			.thenReturn(Optional.of(commandLine));
+		
+		when(commandLine.getOptionValue(CommandLineHelper.CONFIG_FILE_PATH, CommandLineHelper.CONFIG_FILE_DEFAULT_VALUE))
+			.thenReturn(configFilePath);
+
+	}
 }
